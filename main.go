c1633c2613da21ceb73e7b57cc9741da8c8b140d
Kevin Nicky Setiawan <kevin.n.setiawan@banksinarmas.com>
2022-06-30 13:40:03 +0700

    add new mux

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	mux := http.NewServeM1ux()
+	mux := http.NewSer1veM1ux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world!"))
